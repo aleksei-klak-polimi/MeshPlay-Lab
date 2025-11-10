b386e1b161ee5967d69897313bf6dc72fdc355d4
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2025-11-10 13:06:07 +0100

    Added logging and auth routes to server

--- gateway-node/server.js
+++ gateway-node/server.js
@@ -1,15 +1,41 @@
 import express from 'express';
 import config from './config/config.js';
+import morgan from 'morgan';
+import { createLogger } from './config/logger.js';
+import dotenv from 'dotenv';
+import { jsonParserWithValidation, invalidJsonErrorHandler } from './middleware/validateJSON.js';
 
+import authRoutes from './routes/auth.routes.js';
+
+dotenv.config();
+
+const logger = createLogger('server');
 const app = express();
 const port = config.port;
 
+// JSON body parsing
+app.use(jsonParserWithValidation); // replaces app.use(express.json())
+app.use(invalidJsonErrorHandler);
+
+// HTTP request logging (Morgan)
+if (config.env === 'development') {
+  // Detailed dev format
+  app.use(morgan('dev', { stream: logger.stream }));
+} else {
+  // Short format in production
+  app.use(morgan('combined', { stream: logger.stream }));
+}
+
 // Basic health check route
 app.get('/', (req, res) => {
+  logger.debug('Health check called');
   res.send('Server is running!');
 });
 
+// API routes
+app.use('/api/auth', authRoutes);
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+  logger.info(`Server listening on http://localhost:${port}`);
 });
