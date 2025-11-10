150a198119cf270e2206c92424caa3dab452d693
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2025-11-10 20:41:53 +0100

    Renamed user.schemas.js to user.schema.js

--- gateway-node/src/routes/auth.routes.js
+++ gateway-node/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { validateBody } from '../middleware/validate.js';
 import { signup, login } from '../controllers/auth.controller.js';
-import { signupSchema, loginSchema } from '../schemas/user.schemas.js';
+import { signupSchema, loginSchema } from '../schemas/user.schema.js';
 
 const router = express.Router();
 
