a10dc3d0be81c71e11ee309f36369ef96c55d7fe
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2025-11-16 18:21:44 +0100

    Changed timestamp field name

--- gateway-node/src/middleware/requestSignature.middleware.js
+++ gateway-node/src/middleware/requestSignature.middleware.js
@@ -3,6 +3,6 @@ import { randomUUID } from "crypto";
 export const signRequest = (req, res, next) => {
     const requestId = randomUUID();
     const timeStamp = new Date();
-    req.meta = {id: requestId, startTime: timeStamp};
+    req.meta = {id: requestId, timeStamp};
     next();
 };
