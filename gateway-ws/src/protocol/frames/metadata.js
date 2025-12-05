13d08d2f9a9f6d58bc8f8616cc90d379a9df7083
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2025-12-05 16:55:42 +0100

    Removed unused modules

--- gateway-ws/src/protocol/frames/metadata.js
+++
@@ -1,6 +0,0 @@
-export class ResponseMetadata{
-    constructor({serverSideReqId = null, clientSideReqId = null}){
-        this.serverSideReqId = serverSideReqId;
-        this.clientSideReqId = clientSideReqId;
-    }
-}
